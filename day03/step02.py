a85b58a911830f9d2c1cf7b64878e54fae864129
guswlrla <rlaguswl_01@naver.com>
2024-06-25 15:47:04 +0900

    update

--- day03/step02.py
+++ day03/step02.py
@@ -8,27 +8,14 @@ for stepPin in steps:
 	GPIO.setup(stepPin, GPIO.OUT)
 	GPIO.output(stepPin, 0)
 
-stepCounter = 0
-stepCount = 4
-seq = [[0,0,0,1], [0,0,1,0], [0,1,0,0], [1,0,0,0]]
+step_seq = [[0,0,0,1], [0,0,1,0], [0,1,0,0], [1,0,0,0]]
 
 try:
 	while 1:
-		for stepPin in range(0, 4):
-			moter = steps[stepPin]
-			if seq[stepCounter][stepPin] != 0:
-				GPIO.output(moter, True)
-			else:
-				GPIO.output(moter, False)
-
-		stepCounter += 1
-
-		if stepCounter == stepCount:
-			stepCounter = 0
-		if stepCounter < 0:
-			stepCounter = stepCount
-
-		time.sleep(0.01)
+		for seq in step_seq:
+			for pin in range(4):
+				GPIO.output(steps[pin], seq[pin])
+			time.sleep(0.01)
 
 except KeyboardInterrupt:
 	GPIO.cleanup()
