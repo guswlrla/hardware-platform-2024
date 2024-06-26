505c3cf23e1192bc1cf90c3b9bd9bc284709e9e3
guswlrla <rlaguswl_01@naver.com>
2024-06-26 10:19:27 +0900

    update

--- day05/fnd02.py
+++ day05/fnd02.py
@@ -3,28 +3,26 @@ import time
 
 led = [21, 22, 23, 24, 25, 6, 12]
 switch = 26
-count = 1
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(switch, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 for ledPin in led:
 	GPIO.setup(ledPin, GPIO.OUT)
-	GPIO.output(ledPin, False)
+	GPIO.output(ledPin, True)
 
 num = [[1,1,1,1,1,1,0],[0,1,1,0,0,0,0],[1,1,0,1,1,0,1],[1,1,1,1,0,0,1],
 				[0,1,1,0,0,1,1],[1,0,1,1,0,1,1],[1,0,1,1,1,1,1],
 				[1,1,1,0,0,1,0],[1,1,1,1,1,1,1],[1,1,1,1,0,1,1]]
 
+def display(number):
+    for i in range(7):
+        GPIO.output(led[i], num[number][i])
+
 try:
 	while True:
 		if GPIO.input(switch) == True:
-			if count == 9:
-				count = 0 
-			else:
-				count += 1
-
-		for 
+			display()
 
 except KeyboardInterrupt:
 	GPIO.cleanup()
