f7394c9da3b6b3cb43534de0f72024b18e332bd0
guswlrla <rlaguswl_01@naver.com>
2024-06-27 17:30:43 +0900

    update

---
+++ day06/qt02.py
@@ -0,0 +1,27 @@
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+import sys
+
+form_class = uic.loadUiType("./btn.ui")[0]
+
+class WindowClass(QMainWindow, form_class):
+	def __init__(self):
+		super().__init__()
+		self.setupUi(self)
+		# 이벤트함수 등록
+		self.btn_1.clicked.connect(self.btn1Function)
+		self.btn_2.clicked.connect(self.btn2Function)
+
+	def btn1Function(self):
+		print("LED ON Button Clicked!!")
+	def btn2Function(self):
+		print("LED OFF Button Clicked!!")
+
+	def slot1(self):
+		print("EXIT!!")
+
+if __name__ == "__main__":
+	app = QApplication(sys.argv)
+	myWindow = WindowClass()
+	myWindow.show()
+	app.exec_()
